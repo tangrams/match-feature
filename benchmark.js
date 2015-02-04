9b5d0b323045ad2c791d7e3fb87b3cbed8f18355
Ivan Willig <iwillig@gmail.com>
2015-02-04 09:58:04 -0500

    Update benchmarks

--- benchmark.js
+++ benchmark.js
@@ -1,26 +1,10 @@
 'use strict';
 
-function withIn(value, expected, tolerance) {
-    var lessThan = ((expected - tolerance) <= value);
-    if (lessThan === false) {
-        console.log('value is to small');
-        return false;
-    }
-    var gt = ((expected + tolerance) >= value);
-    if (gt === false) {
-        console.log('value is greater than');
-        return false;
-    }
-    return true;
-}
+var expect = require('chai').expect,
+    ff     = require('feature-filter'),
+    match  = require('./index').match;
 
-
-var
-    assert = require('assert'),
-    match = require('./index').match;
-
-var
-    q = { any: [ {any: [ { any: [ { any: [ { kind: 'motorway'}]}]}]} ]},
+var q = { any: [ {any: [ { any: [ { any: [ { kind: 'motorway'}]}]}]} ]},
     context = {
         feature: {
             properties: {
@@ -29,13 +13,55 @@ var
         }
     };
 
+var ffilter = ['any', ['any', ['any', [ 'any', ['==', 'kind', 'motorway']]]]];
 
-var start = new Date();
+function runFF(times) {
+    var start = new Date(), end,
+        filter = ff(ffilter);
 
-for (var i = 0; i < 100000; i += 1) {
-    match(q)(context);
+    for (var i = 0; i < times; i += 1) {
+        filter(context.feature);
+    }
+    end = new Date();
+    return end - start;    
 }
-var end = new Date();
 
+function run(times) {
+    var start = new Date(), end,
+        filter = match(q);
 
-assert.ok(withIn(end - start, 500, 30));
+    for (var i = 0; i < times; i += 1) {
+        filter(context);
+    }
+    end = new Date();
+    return end - start;    
+}
+
+function runTests(times, run) {
+    var runs = [];
+    for (var i = 0; i < times; i += 1) {
+        runs.push(run(1000));
+    }
+    return runs;
+}
+
+function prepResults(runs) {
+    runs = runs.sort(function (a, b) { return a - b; });
+    runs = runs.slice(1);
+    runs = runs.slice(0, runs.length - 1);
+    return runs;
+}
+
+function findAverage(runs) {
+    runs = prepResults(runs);
+    var sum = runs.reduce(function (a, x) { return a + x; }, 0);
+    return sum / runs.length;
+}
+
+var average1 = findAverage(runTests(10000, run));
+var average2 = findAverage(runTests(10000, runFF));
+
+console.log('Average run time for match feature: ' + average1);
+console.log('Average run time for feature filter: ' + average2);
+
+//expect(average1).to.be.closeTo(5, 0.2);
